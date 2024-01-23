5840e9f58dd97401ccfdf55618b70a53f143f6ca
dethblo <dethblo@live.com>
2024-01-23 16:57:06 -0500

    Anonymous functions as passed parameters and as return values.

--- cmd/functions/functions.go
+++ cmd/functions/functions.go
@@ -8,9 +8,15 @@ func main() {
 	numbers := []int{1, 2, 3, 4}
 	doubled := transformNumbers(&numbers, getTransformerFn(false))
 	tripled := transformNumbers(&numbers, getTransformerFn(true))
+	squared := transformNumbers(&numbers, func(number int) int {
+		return number * number
+	})
+	hundred := transformNumbers(&numbers, createTransformer(100))
 
 	fmt.Println(doubled)
 	fmt.Println(tripled)
+	fmt.Println(squared)
+	fmt.Println(hundred)
 }
 
 func transformNumbers(numbers *[]int, transform transformFn) []int {
@@ -35,4 +41,10 @@ func double(number int) int {
 
 func triple(number int) int {
 	return number * 3
+}
+
+func createTransformer(factor int) func(int) int {
+	return func(number int) int {
+		return number * factor
+	}
 }
