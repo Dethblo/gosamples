07fef4f50012a9c6f5663af6f0fe25b3bab8d50e
dethblo <dethblo@live.com>
2024-01-24 14:36:45 -0500

    Reading prices from a file.

--- internal/prices/prices.go
+++ internal/prices/prices.go
@@ -1,6 +1,11 @@
 package prices
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
 type TaxIncludedPriceJob struct {
 	TaxRate           float64
@@ -8,10 +13,14 @@ type TaxIncludedPriceJob struct {
 	TaxIncludedPrices map[string]float64
 }
 
-func (job *TaxIncludedPriceJob) Process() {
-	result := make(map[string]float64)
+func (job TaxIncludedPriceJob) Process() {
+	// load price data from file
+	job.LoadData()
+
+	result := make(map[string]string)
 	for _, price := range job.InputPrices {
-		result[fmt.Sprintf("%.2f", price)] = price * (1 + job.TaxRate)
+		taxIncludedPrice := price * (1 + job.TaxRate)
+		result[fmt.Sprintf("%.2f", price)] = fmt.Sprintf("%.2f", taxIncludedPrice)
 	}
 
 	fmt.Println(result)
@@ -19,7 +28,55 @@ func (job *TaxIncludedPriceJob) Process() {
 
 func NewTaxIncludedPriceJob(taxRate float64) *TaxIncludedPriceJob {
 	return &TaxIncludedPriceJob{
-		InputPrices: []float64{10, 20, 30},
-		TaxRate:     taxRate,
+		TaxRate: taxRate,
 	}
+}
+
+func (job *TaxIncludedPriceJob) LoadData() {
+	file, err := os.Open("prices.txt")
+	if err != nil {
+		fmt.Println("Could not open file!")
+		fmt.Println(err)
+		return
+	}
+
+	// make sure file is closed when done
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			fmt.Println("Could not close file!")
+			fmt.Println(err)
+		}
+	}(file)
+
+	scanner := bufio.NewScanner(file)
+
+	// for each line in the file (until EOF returns false)...
+	var lines []string
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	// check for any error emitted by the scanner
+	err = scanner.Err()
+	if err != nil {
+		fmt.Println("Error reading the file!")
+		fmt.Println(err)
+		return
+	}
+
+	// convert lines to price values
+	prices := make([]float64, len(lines))
+	for lineIndex, line := range lines {
+		floatPrice, conErr := strconv.ParseFloat(line, 64)
+		if conErr != nil {
+			fmt.Printf("Converting string to float failed for %v", line)
+			fmt.Println(err)
+			return
+		}
+
+		prices[lineIndex] = floatPrice
+	}
+
+	job.InputPrices = prices
 }
