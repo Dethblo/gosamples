b24b3e7b2284554a7b6b1ecfaa8033e66a42d53d
dethblo <dethblo@live.com>
2024-01-24 16:06:01 -0500

    Added struct tags to customize the JSON output.

--- cmd/pricecalc/pricecalc.go
+++ cmd/pricecalc/pricecalc.go
@@ -10,7 +10,7 @@ func main() {
 	taxRates := []float64{0, 0.07, 0.1, 0.15}
 
 	for _, taxRate := range taxRates {
-		fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate))
+		fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
 		priceJob := prices.NewTaxIncludedPriceJob(fm, taxRate)
 		priceJob.Process()
 	}
